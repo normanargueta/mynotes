f830a42afcb67a8ebfee3541f08c0e418cebb6e4
Norman Argueta <uchiha.norman@gmail.com>
2023-03-22 09:21:50 -0600

    Improving routing

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -61,7 +63,7 @@ class _LoginViewState extends State<LoginView> {
                 );
 
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/notes/',
+                  notesRoute,
                   (_) => false,
                 );
               } on FirebaseAuthException catch (e) {
@@ -77,7 +79,7 @@ class _LoginViewState extends State<LoginView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/register/',
+                registerRoute,
                 (route) => false,
               );
             },
